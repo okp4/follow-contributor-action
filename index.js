7c113f80676ff9265638c8c97cbf0867e5ba9fe6
Adrien <a.touzouli@okp4.com>
2022-06-28 17:32:18 +0200

    fix: fix api call

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import { getInput, setFailed } from '@actions/core'
-import { getOctokit } from '@actions/github'
+import { getInput, setFailed } from "@actions/core";
+import { getOctokit } from "@actions/github";
 
-run()
+run();
 
 async function run() {
-    try {
-        // Get inputs
-        const token = getInput("token")
-        const username = getInput("username")
+  try {
+    // Get inputs
+    const token = getInput("token");
+    const login = getInput("username");
 
-        result = await getOctokit(token).rest.users.follow(username)
-    } catch (error) {
-        console.log(error)
-        setFailed(error.message)
-    }
+    const result = await getOctokit(token).rest.users.follow({
+      username: login,
+    });
+    console.log("Result : " + result.status);
+  } catch (error) {
+    console.log(error);
+    setFailed(error.message);
+  }
 }
