90b06f3d3b5bb1b4666aa242f52e9ca2ea31fc31
Adrien <a.touzouli@okp4.com>
2022-07-06 08:33:02 +0200

    refactor: rewrite action in typescript

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { getInput, setFailed } from '@actions/core'
+import { getOctokit } from '@actions/github';
+
+async function run(): Promise<void> {
+  try {
+    const token: string = getInput('token');
+    const login: string =  getInput('username');
+
+    const result = await getOctokit(token).rest.users.follow({
+      username: login
+    });
+    console.log('Result : ' + result.status);
+  } catch (error) {
+    if (error instanceof Error) setFailed(error.message)
+  }
+}
+
+run()
