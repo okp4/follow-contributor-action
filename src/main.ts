ea567e24a61f7cd32f34f094073b0db8ae6d6bc3
Adrien <a.touzouli@okp4.com>
2022-08-04 08:33:12 +0200

    style: run prettier and corresponding lint ci job

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { getInput, setFailed } from '@actions/core'
-import { getOctokit } from '@actions/github';
+import {getInput, setFailed} from '@actions/core'
+import {getOctokit} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const token: string = getInput('token');
-    const login: string =  getInput('username');
+    const token: string = getInput('token')
+    const login: string = getInput('username')
 
     const result = await getOctokit(token).rest.users.follow({
       username: login
-    });
-    console.log('Result : ' + result.status);
+    })
+    console.log('Result : ' + result.status)
   } catch (error: unknown) {
     if (error instanceof Error) setFailed(error.message)
   }
