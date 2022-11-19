189516ac39124ae06f3ceb2bfbfc41a634d9f54a
ccamel <camel.christophe@gmail.com>
2022-11-19 22:17:35 +0100

    fix: use proper log level (+ minor output improvements)

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
-import {getInput} from '@actions/core'
-import {getOctokit} from '@actions/github'
+import { getInput } from '@actions/core'
+import { getOctokit } from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const token: string = getInput('token')
     const login: string = getInput('username')
 
+    console.log(`Following user: ${login}`)
+
     const result = await getOctokit(token).rest.users.follow({
       username: login
     })
-    console.log('Result : ' + result.status)
+    
+    console.info(`Result: ${result.status}`)
+
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.log(error.message)
+      console.warn(`Error: ${error.message}`)
     } else {
-      console.log(error)
+      console.warn(`Error`, error)
     }
   }
 }
