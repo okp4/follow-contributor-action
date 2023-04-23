5127bacaf0eb5dc4c21d66a1e662d9f40bcec1a0
ccamel <camel.christophe@gmail.com>
2023-04-23 18:35:31 +0200

    fix: change log level to info

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ const run = async (): Promise<void> => {
     const token: string = getInput('token')
     const username: string = getInput('username')
 
-    console.log(`Following user: ${username}`)
+    console.info(`👀 Following user: ${username}`)
 
     const result = await getOctokit(token).rest.users.follow({
       username
     })
 
-    console.info(`Result: ${result.status}`)
+    console.info(`Result: ${result.status})`)
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.warn(`Error: ${error.message}`)
